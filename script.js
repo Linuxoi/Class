63343efefef18d26198b27ae60526650ca856253
Linux-Penguin-ai <you@example.com>
2020-12-12 16:57:57 +0400

    responsive-fix

--- script.js
+++ script.js
@@ -10,11 +10,20 @@ $(".sub").click(function(){
 		pay = pay + pay*per/100
 		$('#text-div').addClass('text-p')
 		$('#text-div').append('<p>' + "Ваша зарплатa " + pay +".Зарплата поднялась на "+per+"%" + '</p>')
-		$('#text-div').animate({height: '+=60'}, 500);
-		if (clickCount == 9){
-			$('#text-div').animate({height: '+=75'}, 500);
+		$('#text-div').animate({height: '+=75'}, 500);
+		if (clickCount == 5){
+			$('#text-div').animate({height: '+=85'}, 500);
 		}
+		else if (clickCount == 9){
+			$('#text-div').animate({height: '+=95'}, 500);
+		}
+		else if (clickCount == 13){
+			$('#text-div').animate({height: '+=105'}, 500);
+		}		
 		else if (clickCount == 18){
+			$('#text-div').animate({height: '+=110'}, 500);
+		}				
+		else if ($(window).width() < 500 && clickCount == 4){
 			$('#text-div').animate({height: '+=90'}, 500);
 		};
 	});
